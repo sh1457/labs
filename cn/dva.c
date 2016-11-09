dc4606f35df5cd3365e094c1aa22696c4d673a5f
Sujith Sudarshan <sh1457@gmail.com>
2016-11-09 21:23:41 +0530

    Added .gitignore rules for auto-generated files, binary files and input text files

--- cn/dva.c
+++ cn/dva.c
@@ -1,56 +1,45 @@
-/***************************************************************************
-*File		: Dva.c
-*Description: Program to find a suitable path for transmission using distance vector algorithm
-*Author	: Sujith Sudarshan
-*Compiler	: gcc compiler, Ubuntu 14.04
-*Date		: 20 August 2016
-***************************************************************************/
 #include <stdio.h>
+
 struct node {
 	unsigned dist[20];
 	unsigned from[20];
 }rt[10];
-/***************************************************************************
-*Function : main
-*Input parameters : None
-*RETURNS :	0 on success
-***************************************************************************/
+
 int main() {
-  int costmat[20][20], nodes, i, j, k, count=0;
+  int costmat[20][20], nodes, i, j, k, count;
 
-
-  printf("Enter number of nodes : ");
+  printf("Number of Nodes >> ");
   scanf("%d", &nodes);
 
-
-  printf("Enter the cost matrix :\n");
+  printf("Cost matrix >>\n");
   for(i=0; i<nodes; i++) {
     for(j=0; j<nodes; j++) {
       scanf("%d", &costmat[i][j]);
       costmat[i][i] = 0;
-      rt[i].dist[j] = costmat[i][j]; //initialise distance to cost matrix
+      rt[i].dist[j] = costmat[i][j];
       rt[i].from[j] = j;
     }
   }
+
   do {
-    count = 0;
-//We choose arbitrary vertex k and calc the direct distance 
-//from the node i to k using the cost matrix
-//and add the distance from k to node j
-    for(i=0; i<nodes; i++) 
+    for(i=0, count=0; i<nodes; i++)
       for(j=0; j<nodes; j++)
         for(k=0; k<nodes; k++)
-          if(rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) { 
-//We calculate the minimum distance
+          if(rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) {
             rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
             rt[i].from[j] = k;
             count++;
           }
   }while(count != 0);
+
   for(i=0; i<nodes; i++) {
-    printf("\nFor node %d", i+1);
-    for(j=0; j<nodes; j++)
-      printf("\n\tNode %d via %d Distance %d ", j+1, rt[i].from[j]+1, rt[i].dist[j]);
-  }
+    for(j=0; j<nodes; j++) {
+			printf("\tNode %d --> ", i+1);
+			if(j+1 != rt[i].from[j]+1)
+      	printf("Node %d --> ", rt[i].from[j]+1);
+			printf("Node %d  = %d\n", j+1, rt[i].dist[j]);
+		}
+	}
+
   return 0;
 }
